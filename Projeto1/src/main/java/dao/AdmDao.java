e9f667b38d240bffce63b183bb1a0c63e81ae06e
LeciaTamara <leciatamara60@gmail.com>
2026-09-21 09:00:43 -0300

    atualização do no nome das tabelas para a consulta no banco de dados

--- Projeto1/src/main/java/dao/AdmDao.java
+++ Projeto1/src/main/java/dao/AdmDao.java
@@ -11,16 +11,19 @@ import model.Administrador;
 public class AdmDao {
 	//Conexao com o banco
 	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url = "jdbc:mysql://"
-	+ System.getenv("DB_HOST")
-	+ ":"
-	+ System.getenv("DB_PORT")
-	+ "/"
-	+ System.getenv("DB_NAME")
-	+ "?useTimezone=true&serverTimezone=UTC";
+	private String url = "jdbc:mysql://127.0.0.1:3306/pei_db?useTimezone=true&serverTimezone=UTC";
+	private String usuario = "root";
+	private String senha = "lecia2024";
+//	private String url = "jdbc:mysql://"
+//	+ System.getenv("DB_HOST")
+//	+ ":"
+//	+ System.getenv("DB_PORT")
+//	+ "/"
+//	+ System.getenv("DB_NAME")
+//	+ "?useTimezone=true&serverTimezone=UTC";
 	
-	private String usuario = System.getenv("DB_USER");
-	private String senha = System.getenv("DB_PASSWORD");
+//	private String usuario = System.getenv("DB_USER");
+//	private String senha = System.getenv("DB_PASSWORD");
 	
 	//metodo de conexao
 	
--- Projeto1/src/main/java/dao/EmpresaDao.java
+++ Projeto1/src/main/java/dao/EmpresaDao.java
@@ -12,16 +12,19 @@ public class EmpresaDao {
 	
 	//Conexao com o banco
 	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url = "jdbc:mysql://"
-			+ System.getenv("DB_HOST")
-			+ ":"
-			+ System.getenv("DB_PORT")
-			+ "/"
-			+ System.getenv("DB_NAME")
-			+ "?useTimezone=true&serverTimezone=UTC";
-			
-			private String usuario = System.getenv("DB_USER");
-			private String senha = System.getenv("DB_PASSWORD");	
+	private String url = "jdbc:mysql://127.0.0.1:3306/pei_db?useTimezone=true&serverTimezone=UTC";
+	private String usuario = "root";
+	private String senha = "lecia2024";
+//	private String url = "jdbc:mysql://"
+//			+ System.getenv("DB_HOST")
+//			+ ":"
+//			+ System.getenv("DB_PORT")
+//			+ "/"
+//			+ System.getenv("DB_NAME")
+//			+ "?useTimezone=true&serverTimezone=UTC";
+//			
+//			private String usuario = System.getenv("DB_USER");
+//			private String senha = System.getenv("DB_PASSWORD");	
 	//metodo de conexao
 		
 	private Connection conectar() {
